a29b9d4ccf1f636209e6d114b6f2c0932009a54f
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-04 18:54:02 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='m-2 bg-red-500'>
+      <h1 className='text-green-500'>Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
