d5c6d27749c89e63e03d09c6aaca263d99147090
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-09 23:08:30 +0100

    "Initial commit"

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,30 @@
+import { useId } from "react";
+const Input = React.forwardRef( function Input({
+    label, 
+    type = "text", 
+    className = "", 
+    ...props}, ref) 
+    {
+        const id = useId()
+        return (
+            <div className="w-full">
+                {label && (
+                    <label
+                    htmlFor={id}
+                    className="inline-block mb-1 pl-1"
+                    >
+                        {label}
+                    </label>
+                )}
+                <input 
+                type={type}
+                ref={ref}
+                {...props}
+                id={id}
+                className={``}
+                 />
+            </div>
+        )
+    })
+ 
+export default Input;
