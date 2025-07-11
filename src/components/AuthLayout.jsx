e4bda6033dff6eaed3e1ab15c660a4588efebb26
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-11 14:14:23 +0100

    "Initial commit"

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+
+const AuthLayout = ({children, authentication = true}) => {
+
+    const authStatus = useSelector((state) => state.auth.status)
+
+    const navigate = useNavigate()
+    const[loader, setLoader] = useState(true)
+
+    useEffect(() => {
+        if (authentication && authStatus !== authentication){
+            navigate("/login")
+
+        } // logic to check if user is logged in
+        else if (!authentication && authStatus !== authentication) {
+            navigate("/")
+        }
+        setLoader(false)
+    }, [authStatus, authentication, navigate])
+
+    return loader ? <div><h1>Loading...</h1></div> : <>{children}</>
+}
+ 
+export default AuthLayout;
