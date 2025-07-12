9446a07655462fd4d05a050514c5f0ec88f747be
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-12 15:50:09 +0100

    Version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import { useDispatch } from 'react-redux'
+import { login, logout } from "./store/authSlice";
+import { useEffect } from 'react'
+import authService from './appwrite/auth'
 
 function App() {
+
+    const dispatch = useDispatch();
+
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const user = await authService.getCurrentUser(); // ✅ If logged in, returns user
+        dispatch(login(user));            // 🔁 Restore auth state
+      } catch (err) {
+        dispatch(logout());               // ❌ No session, ensure logout
+      }
+    };
+
+    checkSession();
+  }, []);
 
   return (
     <>
     <div>
+      <Header/>
       <main>
         <Outlet />
       </main>
+      <Footer/>
     </div>
     </>
   )
