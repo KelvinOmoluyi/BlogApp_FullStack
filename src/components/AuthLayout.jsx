c3a6fef9ade832192a10832c88fc46d3004ef269
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-12 15:51:01 +0100

    Version 2

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,27 +1,32 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const AuthLayout = ({children, authentication = true}) => {
 
-    const authStatus = useSelector((state) => state.auth.status)
+  const authStatus = useSelector((state) => state.auth.status )
 
-    const navigate = useNavigate()
-    const[loader, setLoader] = useState(true)
+  const navigate = useNavigate()
+  const [loader, setLoader] = useState(true)
 
-    useEffect(() => {
-        if (authentication && authStatus !== authentication){
-            navigate("/login")
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login")
+    } else if (!authentication && authStatus !== authentication ){
+      navigate("/")
+    }
+    setLoader(false)
+  }, [authStatus, authentication, navigate])
 
-        } // logic to check if user is logged in
-        else if (!authentication && authStatus !== authentication) {
-            navigate("/")
-        }
-        setLoader(false)
-    }, [authStatus, authentication, navigate])
-
-    return loader ? <div><h1>Loading...</h1></div> : <>{children}</>
+  return loader ? null : <>{children}</>
 }
- 
-export default AuthLayout;
+
+export default AuthLayout
+
+
+// if(true){
+//   if (false) {
+//     navigator("/login")
+//   }
+// }
