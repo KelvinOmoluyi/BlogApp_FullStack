284de13aca3617b543bd2ae54fd4ded807b541c8
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-14 09:27:13 +0100

    fixed image display bug

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 import appwriteService from "../appwrite/config.js"
 
-const postCard = ({$id, title, featuredImage}) => {
+const postCard = ({$id, title, featured_img}) => {
     return (
         <Link to={`/post/${$id}`}>
             <div
-            className='w-full bg-gray-100 rounded-xl p-4'
+            className='w-75 bg-gray-100 rounded-xl p-4'
             >
                 <div
                 className='w-full justify-center mb-4'
                 >
-                    <img src={appwriteService.getFilePreview($id)} alt={title}
-                    className='rounded-xl'
+                    <img src={appwriteService.getFileView(featured_img)} alt={title}
+                    className='rounded-xl h-50'
                     />
                 </div>
                 <h2 className='text-xl font-bold'>{title}</h2>
