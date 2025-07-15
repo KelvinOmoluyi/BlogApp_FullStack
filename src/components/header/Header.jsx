e2acb41ba48b002caa733f13014c21f94069fa67
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-15 12:29:24 +0100

    updating syle

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
     ]
 
   return (
-    <header className='py-3 shadow bg-gray-500'>
+    <header className='py-3 min-w-full shadow rounded-[30px] bg-yellow-500'>
         <Container>
             <nav className='flex'>
                 <div className='mr-4'>
@@ -53,7 +53,7 @@ const Header = () => {
                             <li key={item.name}>
                                 <button
                                 onClick={() => navigate(item.slug)}
-                                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                                className='inline-bock px-6 py-2 duration-200 hover:bg-yellow-600 rounded-full'
                                 >
                                     {item.name}
                                 </button>
