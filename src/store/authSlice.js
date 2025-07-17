4ff5cbf39f2d56d30e5dd1b8a4b62b758ba24de0
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-17 21:40:47 +0100

    added loading state

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -2,7 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     status: false,
-    userData: null
+    userData: null,
+    loading: true
 }
 
 const authSlice = createSlice({
@@ -12,14 +13,19 @@ const authSlice = createSlice({
         login: (state, action) => {
             state.status = true;
             state.userData = action.payload
+            state.loading = false;
         },
         logout: (state) => {
             state.status = false;
             state.userData = null;
+            state.loading = false;
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload;
         }
     }
 })
 
-export const { login, logout } = authSlice.actions
+export const { login, logout, setLoading } = authSlice.actions
 
 export default authSlice.reducer
