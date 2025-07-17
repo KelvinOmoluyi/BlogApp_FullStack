0c0c262221d768833039f05ebf389bedbc482205
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-07-17 21:39:43 +0100

    popularized with code

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,7 +1,34 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import appwriteService from "../appwrite/config"
+import Container from "../components/container/Container";
+import PostForm from "../components/post-form/PostForm"
+
+
 const EditPost = () => {
+    const [post, setPost] = useState("")
+    const{slug} = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (slug) {
+        appwriteService.getPost(slug).then((post) => {
+            if (post) {
+            setPost(post)
+            } else {
+            navigate("/")
+            }
+        })
+        }
+    }, [slug, navigate])
+
     return (
-        <h1>Edit posts</h1>
-    );
-}
+        <div className='py-6'>
+            <Container>
+                <PostForm post={post} />
+            </Container>
+        </div>
+    )
+    }
  
 export default EditPost;
